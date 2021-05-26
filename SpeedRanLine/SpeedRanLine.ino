d527de795568b10ba2a2e0c740b1d04f8a65d2ca
Dimas <dima.sikora.97@mail.ru>
2021-05-26 18:59:51 +0300

    Add SpeedRanLine

---
+++ SpeedRanLine/SpeedRanLine.ino
@@ -0,0 +1,87 @@
+int timer;
+
+const int RIGHT_SENCE = 14;
+const int MIDDLE_SENCE = 15;
+const int LEFT_SENCE = 16;
+
+const int RightMotorSpeed = 100;
+const int LeftMotorSpeed = 100;
+
+const int RightMotorPin = 9;
+const int RightMotorIn1 = 7;
+const int RightMotorIn2 = 6;
+const int LeftMotorPin = 3;
+const int LeftMotorIn1 = 5;
+const int LeftMotorIn2 = 4;
+
+void setup() {
+  Serial.begin (9600);
+  pinMode (RIGHT_SENCE, INPUT);
+  pinMode (MIDDLE_SENCE, INPUT);
+  pinMode (LEFT_SENCE, INPUT);
+
+  analogWrite (RightMotorPin, RightMotorSpeed);
+  analogWrite (LeftMotorPin, LeftMotorSpeed);
+  pinMode (RightMotorIn1, OUTPUT);
+  pinMode (RightMotorIn2, OUTPUT);
+  pinMode (LeftMotorIn1, OUTPUT);
+  pinMode (LeftMotorIn2, OUTPUT);
+}
+
+void loop() {
+  
+  bool rightSenseState = !digitalRead (RIGHT_SENCE);
+  bool middleSenseState = !digitalRead (MIDDLE_SENCE);
+  bool leftSenseState = !digitalRead (LEFT_SENCE);
+  Serial.print("Right sensor: ");
+  Serial.print (rightSenseState);
+  Serial.print(" Middle sensor: ");
+  Serial.print (middleSenseState);
+  Serial.print(" Left sensor: ");
+  Serial.println (leftSenseState);
+
+  if (leftSenseState == 1 && middleSenseState == 0 && rightSenseState == 0) { left (); } // - 100 влево
+    
+  else if (leftSenseState == 0 && middleSenseState == 0 && rightSenseState == 1) { right(); } // 001 - вправо
+    
+  else if (leftSenseState == 1 && middleSenseState == 1 && rightSenseState == 0) { right(); } // 110 - вправо
+  
+  else if (leftSenseState == 0 && middleSenseState == 1 && rightSenseState == 1) { left(); } // 011 - влево
+  
+  else if (leftSenseState == 1 && middleSenseState == 1 && rightSenseState == 1) { left(); } // 111 - влево
+  
+  else if (leftSenseState == 0 && middleSenseState == 0 && rightSenseState == 0) { // 000 - назад
+    backward();
+    timer = millis ();
+  } 
+
+  else   { forward (); }
+}
+
+void forward () {
+  digitalWrite (RightMotorIn1, LOW);
+  digitalWrite (RightMotorIn2, HIGH);
+  digitalWrite (LeftMotorIn1, LOW);
+  digitalWrite (LeftMotorIn2, HIGH);
+}
+
+void backward () {
+  digitalWrite (RightMotorIn1, HIGH);
+  digitalWrite (RightMotorIn2, LOW);
+  digitalWrite (LeftMotorIn1, HIGH);
+  digitalWrite (LeftMotorIn2, LOW);
+}
+
+void left () {
+  digitalWrite (RightMotorIn1, HIGH);
+  digitalWrite (RightMotorIn2, LOW);
+  digitalWrite (LeftMotorIn1, LOW);
+  digitalWrite (LeftMotorIn2, HIGH);
+}
+
+void right () { 
+  digitalWrite (RightMotorIn1, LOW);
+  digitalWrite (RightMotorIn2, HIGH);
+  digitalWrite (LeftMotorIn1, HIGH);
+  digitalWrite (LeftMotorIn2, LOW);
+}
